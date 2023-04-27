a8081d1220d52e71d026dad31b62e1eef143469b
Florent <f@linguenheld.fr>
2023-04-27 21:29:02 +0200

    Test current cpu utilization

--- include/process.h
+++ include/process.h
@@ -12,7 +12,7 @@ class Process {
         int Pid() const;
         std::string User();
         std::string Command();
-        float CpuUtilization() const;                  // TODO: See src/process.cpp
+        float CpuUtilization();                  // TODO: See src/process.cpp
         std::string Ram() const;
         long int UpTime();
         bool operator<(Process const& a) const;  // TODO: See src/process.cpp
@@ -21,6 +21,20 @@ class Process {
     private:
         std::string _pid;
         std::string _path;
+
+
+        void update_process_values(float &utime, float &stime);
+        float _utime_0;
+        float _utime_1;
+
+        float _stime_0;
+        float _stime_1;
+
+        void update_proc_value(float &time_total);
+        float _time_total_0;
+        float _time_total_1;
+
+        float _cpu_utilization;
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -16,43 +16,84 @@ using std::vector;
 
 
 Process::Process(std::string pid) : _pid(pid), _path("/proc/" + pid)
-{};
+{
+};
 
 int Process::Pid() const {
     return std::stoi(_pid);
 }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() const {
+float Process::CpuUtilization() {
 
-    try {
-        std::vector<std::string> fields = LinuxParser::parse(_path + "/stat",
-                                                             std::vector<int>{13, 14, 15, 16, 21});
+    // try {
+    //     std::vector<std::string> fields = LinuxParser::parse(_path + "/stat",
+    //                                                          std::vector<int>{13, 14, 15, 16, 21});
 
-        auto uptime = std::stof(LinuxParser::parse("/proc/uptime", 0));
+    //     auto uptime = std::stof(LinuxParser::parse("/proc/uptime", 0));
 
-        auto utime = std::stof(fields[0]);
-        auto stime  = std::stof(fields[1]);
-        auto cutime = std::stof(fields[2]);
-        auto cstime = std::stof(fields[3]);
-        auto starttime = std::stof(fields[4]);
+    //     auto utime = std::stof(fields[0]);
+    //     auto stime  = std::stof(fields[1]);
+    //     auto cutime = std::stof(fields[2]);
+    //     auto cstime = std::stof(fields[3]);
+    //     auto starttime = std::stof(fields[4]);
 
-        auto Hertz = sysconf(_SC_CLK_TCK);
+    //     auto Hertz = sysconf(_SC_CLK_TCK);
 
-        auto total_time = utime + stime;
-        total_time = total_time + cutime + cstime;
-        auto seconds = uptime - (starttime / Hertz);
-        auto cpu_usage = (total_time / Hertz) / seconds;
+    //     auto total_time = utime + stime;
+    //     total_time = total_time + cutime + cstime;
+    //     auto seconds = uptime - (starttime / Hertz);
+    //     auto cpu_usage = (total_time / Hertz) / seconds;
 
-        return cpu_usage;
-    }
-    catch (const std::bad_alloc &ia) {
-        return 0.0;
-    }
-    catch (const std::invalid_argument &ia) {
-        return 0.0;
-    }
+    //     return cpu_usage;
+    // }
+    // catch (const std::bad_alloc &ia) {
+    //     return 0.0;
+    // }
+    // catch (const std::invalid_argument &ia) {
+    //     return 0.0;
+    // }
+
+    update_process_values(_utime_0, _stime_0);
+    update_proc_value(_time_total_0);
+    usleep(50000);
+    update_process_values(_utime_1, _stime_1);
+    update_proc_value(_time_total_1);
+
+    auto user_util = sysconf(_SC_NPROCESSORS_ONLN) * (_utime_1 - _utime_0) / (_time_total_1 - _time_total_0);
+    auto sys_util = sysconf(_SC_NPROCESSORS_ONLN) * (_stime_1 - _stime_0) / (_time_total_1 - _time_total_0);
+
+    _cpu_utilization = user_util + sys_util;
+    return _cpu_utilization;
 }
+
+
+void Process::update_process_values(float &utime, float &stime)
+{
+    std::vector<std::string> fields_process = LinuxParser::parse(_path + "/stat",
+                                                         std::vector<int>{13, 14});
+    utime = std::stof(fields_process[0]);
+    stime = std::stof(fields_process[1]);
+}
+
+void Process::update_proc_value(float &time_total)
+{
+    std::vector<std::string> fields_cpu = LinuxParser::parse("/proc/stat",
+                                                         std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9});
+    time_total = 0;
+    for (auto f : fields_cpu)
+        time_total += std::stof(f);
+}
+
+
+
+
+
+
+
+
+
+
 
 string Process::Command() {
     return LinuxParser::parse(_path + "/cmdline", 0);
@@ -75,13 +116,15 @@ string Process::User() {
 }
 
 long int Process::UpTime() {
-    // auto field = std::stol(LinuxParser::parse(_path + "/stat", 21));
-    auto field = std::stol(LinuxParser::parse(_path + "/stat", 13));
-    auto sec = field / sysconf(_SC_CLK_TCK);
+    auto field = std::stol(LinuxParser::parse(_path + "/stat", 21));
+    auto process_start = field / sysconf(_SC_CLK_TCK);
 
-    return sec;
+    auto os_start = std::stol(LinuxParser::parse(LinuxParser::kUptimeFilename, 0));
+
+    return os_start - process_start;
 }
 
 bool Process::operator<(Process const& a) const {
-    return CpuUtilization() < a.CpuUtilization();
+    return Pid() < a.Pid();
+    return _cpu_utilization > a._cpu_utilization;
 }
